be10f33a5cccb8dcc720652d9e9f8c50fee70d8e
Rasmus Lindholm <124736358+lira21ol@users.noreply.github.com>
2024-11-13 17:03:36 +0100

    ny android studio med test kod

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,22 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.comrasmusfishingwithfriends"
-    compileSdk = 34
+
+
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.comrasmusfishingwithfriends"
-        minSdk = 34
-        targetSdk = 34
+
+
+        minSdk = 26
+        targetSdk = 35
+
         versionCode = 1
         versionName = "1.0"
 
@@ -29,19 +35,24 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildFeatures {
         compose = true
     }
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -50,19 +61,21 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.activity:activity-compose:1.9.3")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.firebase:firebase-firestore")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.10.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
--- app/src/main/java/com/example/comrasmusfishingwithfriends/MainActivity.kt
+++ app/src/main/java/com/example/comrasmusfishingwithfriends/MainActivity.kt
@@ -3,44 +3,101 @@ package com.example.comrasmusfishingwithfriends
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.comrasmusfishingwithfriends.ui.theme.ComrasmusfishingwithfriendsTheme
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.toObjects
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+data class Player(
+    val playerId: String = "",
+    var name: String = "",
+    var invitation: String = "",
+    var score: Int = 0
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            ComrasmusfishingwithfriendsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("test")
-                }
+            ComrasmusfishingwithfriendsTheme  {
+
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen() {
+    val db = Firebase.firestore
+    val playerList = MutableStateFlow<List<Player>>(emptyList())
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComrasmusfishingwithfriendsTheme {
-        Greeting("Android")
+    db.collection("players")
+        .addSnapshotListener{ value, error ->
+            if (error != null) {
+                return@addSnapshotListener
+            }
+            if (value != null) {
+                playerList.value = value.toObjects()
+            }
+        }
+
+    val players by playerList.asStateFlow().collectAsStateWithLifecycle()
+
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        LazyColumn(modifier = Modifier.padding(innerPadding)) {
+            items(players) { player ->
+
+                ListItem(
+                    headlineContent = {
+                        Text("Name: ${player.name}")
+                    },
+                    supportingContent = {
+                        Text("Score: ${player.score}")
+                    },
+                    trailingContent = {
+                        Button(onClick = {
+                            val query = db.collection("players").whereEqualTo("playerId", player.playerId)
+
+                            query.get().addOnSuccessListener { querySnapshot ->
+                                for (documentSnapshot in querySnapshot) {
+                                    documentSnapshot.reference.update("invitation", "Hello!")
+                                }}
+
+
+
+                        }) {
+                            Text("Invite")
+                        }
+                    }
+                )
+
+
+            }
+        }
     }
 }
+
+
+
+
+
